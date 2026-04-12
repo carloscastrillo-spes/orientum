fd0138b6394c6d9001bc4c7e4bc48f04856d4732
carloscastrillo-spes <carlos.castrillo@castrillo.com.ar>
2026-04-12 18:32:03 -0300

    Update oracle.js

--- netlify/functions/oracle.js
+++ netlify/functions/oracle.js
@@ -1,10 +1,10 @@
 exports.handler = async (event) => {
   return {
-    statusCode\: 200,
-    headers\: {
-      "Content-Type"\: "application/json",
-      "Access-Control-Allow-Origin"\: "*"
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
-    body\: JSON.stringify({ respuesta\: "función activa" })
+    body: JSON.stringify({ respuesta: "función activa" })
   };
 };
